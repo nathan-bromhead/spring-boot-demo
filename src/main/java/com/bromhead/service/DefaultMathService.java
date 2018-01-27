c018652616a265d876c33c7b79a288487fe2c4c6
nathan@nathan-dsk <nathan@nathan-dsk>
2018-01-27 09:59:01 -0700

    updating math service test

--- src/main/java/com/bromhead/service/DefaultMathService.java
+++ src/main/java/com/bromhead/service/DefaultMathService.java
@@ -2,15 +2,25 @@ package com.bromhead.service;
 
 import java.math.BigDecimal;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class DefaultMathService implements IMathService {
 
 	@Override
 	public BigDecimal add(String param1, String param2) {
+		validateParamIsNumber(param1);
+		validateParamIsNumber(param2);
 		
 		BigDecimal operand1 = new BigDecimal(param1);
 		BigDecimal operand2 = new BigDecimal(param2);
 		
 		return operand1.add(operand2);
 	}
+	
+	private void validateParamIsNumber(String param) {
+		if (!NumberUtils.isNumber(param))
+			throw new IllegalArgumentException(param + " is not a valid numeric value");
+	}
+	
 	
 }
--- src/test/java/com/bromhead/unit/MathServiceTest.java
+++ src/test/java/com/bromhead/unit/MathServiceTest.java
@@ -1,6 +1,8 @@
 package com.bromhead.unit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.math.BigDecimal;
 
@@ -38,6 +40,55 @@ public class MathServiceTest {
 		
 		BigDecimal actualResult = service.add(param1, param2);
 		
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	public void testAdd_invalidParam1() {
+		String param1 = "not_a_number";
+		String param2 = "15";
+		
+		try {
+			service.add(param1, param2);
+			fail("Test should have failed");
+		} catch (Exception e) {
+			assertTrue(e instanceof IllegalArgumentException);
+		}
+	}
+	
+	@Test
+	public void testAdd_invalidParam2() {
+		String param1 = "10";
+		String param2 = "not_a_number";
+		
+		try {
+			service.add(param1, param2);
+			fail("Test should have failed");
+		} catch (Exception e) {
+			assertTrue(e instanceof IllegalArgumentException);
+		}
+	}
+	
+	@Test
+	public void testAdd_LargeIntegers() {
+		String param1 = "1000000000000000000000000000000000000000000000000000000000000000000000000";
+		String param2 = "1500000000000000000000000000000000000000000000000000000000000000000000000";
+		BigDecimal expectedResult = new BigDecimal("2500000000000000000000000000000000000000000000000000000000000000000000000");
+		
+		BigDecimal actualResult = service.add(param1, param2);
+		
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	public void testAdd_LargeFloatingPoint() {
+		String param1 = "789652325698542135.326589654784712596";
+		String param2 = "9545848565489951751.365845145970540145740";
+		
+		BigDecimal expectedResult = new BigDecimal("10335500891188493886.692434800755252741740");
+
+		BigDecimal actualResult = service.add(param1, param2);
+
 		assertEquals(expectedResult, actualResult);
 	}
 
